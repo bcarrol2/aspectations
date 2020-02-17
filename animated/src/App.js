9662799c19d71c287171558278eb7578d4d8a06e
Blair <blairacarroll@gmail.com>
2020-02-16 21:33:02 -0500

    new image, moving div, other style changes

--- animated/src/App.js
+++ animated/src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
           in
           appear
           duration={3000}
+          // loop={3}
           onEntering={{ translateX: [{value: 15}, {value: '+=0'}], translateY: [{value: 0}, {value: 100}], opacity: [0, 0.7] }}
           onExiting={{ translateY: -20, opacity: 0.4 }}
           easing="easeOutCubic"
@@ -22,6 +23,21 @@ class App extends React.Component {
           </section>
           <section id="box">
             <p> Word3 </p>
+          </section>
+        </Anime>
+        <Anime
+          in
+          appear
+          duration={1000}
+          onEntering={{ translateX: [{value: 15}, {value: '+=0'}], translateY: [{value: 0}, {value: 100}], opacity: [0, 0.7] }}
+          easing="easeOutCubic"
+        >
+          <section id="bottom-box">
+            <div className="centered">
+              <div className="moving">
+                <h1>Hey</h1>
+              </div>
+            </div>
           </section>
         </Anime>
       </main>
