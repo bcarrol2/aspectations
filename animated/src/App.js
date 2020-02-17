58ee4651372cbf2fbe7d261e78259cb94472a479
Blair <blairacarroll@gmail.com>
2020-02-16 22:46:58 -0500

    deleted readme, style changes, component deleted

--- animated/src/App.js
+++ animated/src/App.js
@@ -6,6 +6,9 @@ class App extends React.Component {
   render() {
     return (
       <main>
+        <div id="tech">
+          <h1 style={{color: 'white', fontSize: '75px'}}>Techie</h1>
+        </div>
         <Anime
           in
           appear
@@ -16,13 +19,13 @@ class App extends React.Component {
           easing="easeOutCubic"
         >
           <section id="box">
-            <p> Word </p>
+            <p>Git Pull</p>
           </section>
           <section id="box">
-            <p> Word2 </p>
+            <p>Git Push</p>
           </section>
           <section id="box">
-            <p> Word3 </p>
+            <p>Git Up</p>
           </section>
         </Anime>
         <Anime
@@ -30,12 +33,12 @@ class App extends React.Component {
           appear
           duration={1000}
           onEntering={{ translateX: [{value: 15}, {value: '+=0'}], translateY: [{value: 0}, {value: 100}], opacity: [0, 0.7] }}
-          easing="easeOutCubic"
+          easing = "steps(3)"
         >
           <section id="bottom-box">
             <div className="centered">
               <div className="moving">
-                <h1>Hey</h1>
+                <h1>My new year's resolution is 2387 x 1704</h1>
               </div>
             </div>
           </section>
